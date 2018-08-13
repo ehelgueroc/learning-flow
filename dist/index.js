378dca32d057e9d3c208f8a91b6b78ebbd5a4336
Esteban Helguero <esteban.helguero@redk.net>
2018-08-14 01:33:28 +0200

    Functions with flow

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,12 @@ let name = 'John Doe';
 let age = 40;
 let something = [1, 2, 3];
 let users = ['John', 'Sara'];
+let ids = [1, 2, 3];
+
+let person;
+
+person = {
+	name: 'John',
+	age: 30,
+	hasKid: false
+};
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ let something: any = [1,2,3];
 let users: string[] = ['John', 'Sara'];
 let ids: number[] = [1,2,3];
 
+// INTERFACE
 let person: Person;
 
 person = {
@@ -20,3 +21,11 @@ interface Person {
 	age: number;
 	hasKids?: boolean;
 }
+
+// FUNCTIONS
+
+function greeting(name: string): string{
+	return 'Hello ' + name;
+}
+
+console.log(greeting(1));
