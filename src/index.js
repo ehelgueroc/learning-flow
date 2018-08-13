ef5e1454289a785f134dbd7978cb06d527d0123e
Esteban Helguero <esteban.helguero@redk.net>
2018-08-14 01:31:11 +0200

    Created an interface and flow optionals

--- src/index.js
+++ src/index.js
@@ -5,4 +5,18 @@ let name: string = 'John Doe';
 let age: number = 40;
 let something: any = [1,2,3];
 let users: string[] = ['John', 'Sara'];
-let ids: number[] = [1,2,3, true];
+let ids: number[] = [1,2,3];
+
+let person: Person;
+
+person = {
+	name: 'John',
+	age: 30,
+	hasKid: false
+}
+
+interface Person {
+	name: string;
+	age: number;
+	hasKids?: boolean;
+}
